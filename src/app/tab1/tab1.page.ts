7b6466d6e800fc716596820bd1fde4b92b24a827
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2021-09-15 20:21:14 +0530

    Ionic try slogify leave approoval page base

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { HeaderColor } from '@ionic-native/header-color/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
+
+  // this.headerColor.tint('#becb29');
 
 }
